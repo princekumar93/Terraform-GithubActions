f1846c4291f7994da9f70f688ededa0c3c7254b3
Prince Kumar <89452166+princekumar93@users.noreply.github.com>
2022-11-20 18:02:02 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,12 +6,15 @@ locals {
   bucket_name = "test-terraform-1234-bucket"
 }
 
+variable "workspacePath" {
+}
+
 resource "aws_s3_bucket_object" "file_upload" {
   bucket = local.bucket_name
-  for_each = fileset("/DataFiles/", "*")
+  for_each = fileset("${workspacePath}/DataFiles/", "*")
   key    = "github//${each.value}"
-  source = "/DataFiles//${each.value}"
-  etag = filemd5("/DataFiles//${each.value}")
+  source = "${workspacePath}/DataFiles//${each.value}"
+  etag = filemd5("${workspacePath}/DataFiles//${each.value}")
 }
 
 output "filecount" {
