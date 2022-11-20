44c91f960d3776fa315d299ae4c13e39d414c91a
Prince Kumar <89452166+princekumar93@users.noreply.github.com>
2022-11-20 16:07:01 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,9 +7,9 @@ locals {
 }
 
 resource "aws_s3_bucket_object" "object" {
-  bucket = "test-terraform-1234-bucket"
+  bucket = local.bucket_name
   for_each = fileset("DataFiles/", "*")
   key    = "github//${each.value}"
-  source = "DataFiles/${each.value}"
-  etag = filemd5("DataFiles/${each.value}")
+  source = "DataFiles//${each.value}"
+  etag = filemd5("DataFiles//${each.value}")
 }
