0d7092c502d0fc466f5f88ede01926dc2d1d1929
Prince Kumar <89452166+princekumar93@users.noreply.github.com>
2022-11-20 15:46:23 +0530

    Update S3Upload.tf

--- Terraform/S3Upload.tf
+++ Terraform/S3Upload.tf
@@ -8,8 +8,8 @@ locals {
 
 resource "aws_s3_bucket_object" "object" {
   bucket = "test-terraform-1234-bucket"
-  for_each = fileset("Terraform-GithubActions/DataFiles", "*")
-  key    = "github/${each.value}"
-  source = "Terraform-GithubActions/DataFiles/${each.value}"
-  etag = filemd5("Terraform-GithubActions/DataFiles/${each.value}")
+  for_each = fileset("Terraform-GithubActions//DataFiles", "*")
+  key    = "github//${each.value}"
+  source = "Terraform-GithubActions//DataFiles//${each.value}"
+  etag = filemd5("Terraform-GithubActions//DataFiles//${each.value}")
 }
