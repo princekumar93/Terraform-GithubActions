6f22ee3d70048e1f58e82562915cdc656965ee24
Prince Kumar <89452166+princekumar93@users.noreply.github.com>
2022-11-20 15:30:20 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,10 +3,14 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "testInstance" {
-  ami = "ami-02b972fec07f1e659"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "terraformEC2"
-  }
+locals {
+  bucket_name = "test-terraform-1234-bucket"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = local.bucket_name
+  for_each = fileset("Terraform-GithubActions/DataFiles/", "*")
+  key    = "github/${each.value}"
+  source = "Terraform-GithubActions/DataFiles/${each.value}"
+  etag = filemd5("Terraform-GithubActions/DataFiles/${each.value}")
 }
