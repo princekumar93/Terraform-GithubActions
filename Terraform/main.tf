7a3d4b1bb248751520937f4660d61bfc8e5880ee
Prince Kumar <89452166+princekumar93@users.noreply.github.com>
2022-11-30 13:45:38 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_object" "file_upload" {
   bucket = "test-terraform-1234-bucket-prod"
   for_each = fileset("../DataFiles/", "**")
   key    = "github//${each.value}"
-  source = "$../DataFiles//${each.value}"
+  source = "../DataFiles//${each.value}"
   etag = filemd5("../DataFiles//${each.value}")
 }
 
